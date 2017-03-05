a79e565c3f9e94d26243dedbe11547f415f3d38f
KevinMidboe <kevin.midboe@gmail.com>
2017-03-05 15:25:00 +0100

    Now renames everything and gets location of original item and new location for item

--- folderCreator.py
+++ folderCreator.py
@@ -3,9 +3,9 @@
 # @Author: KevinMidboe
 # @Date:   2017-03-05 13:52:45
 # @Last Modified by:   KevinMidboe
-# @Last Modified time: 2017-03-05 14:59:26
+# @Last Modified time: 2017-03-05 15:22:30
 
-import sqlite3, json
+import sqlite3, json, os
 from re import sub
 
 dbPath = 'shows.db'
@@ -32,36 +32,53 @@ def unpackEpisodes():
 
 
 def createFolders(episode):
-	
+	showDir = '/Volumes/media/tv/%s/'% episode['name']
+	episodeFormat = '%s S%sE%s/'% (episode['name'], episode['season'], episode['episode'])
+	seasonFormat = '%s Season %s/'% (episode['name'], episode['season'])
+
+	if not os.path.isdir(showDir + seasonFormat):
+		os.makedirs(showDir + seasonFormat)
+
+	if not os.path.isdir(showDir + seasonFormat + episodeFormat):
+		os.makedirs(showDir + seasonFormat + episodeFormat)
 
 
-def getMediaitems(media_items):
+
+def newnameMediaitems(media_items):
 	# media_items = [['New.Girl.S06E18.720p.HDTV.x264-EZTV.srt', '-EZTV', 'nl'], ['New.Girl.S06E18.720p.HDTV.x264-FLEET.srt', '-FLEET', 'en']]
 	media_items = json.loads(media_items)
 
 	returnList = []
 	for item in media_items:
-		returnList.append(sub(item[1], '', item[0]))
+		returnList.append([item[0], sub(item[1], '', item[0])])
 
 	return returnList
 
-def getSubtitles(subtitles):
+def newnameSubtitles(subtitles):
 	subtitles = json.loads(subtitles)
 
 	returnList = []
 	for item in subtitles:
-		returnList.append(sub(item[1], '.' + item[2], item[0]))
+		returnList.append([item[0], sub(item[1], '.' + item[2], item[0])])
 
 	return returnList
 
 
 def moveFiles(episode):
-	newMediaitems = getMediaitems(episode['media_items'])
-	print(newMediaitems)
+	showDir = '/Volumes/media/tv/'
+	episodeFormat = '%s S%sE%s/'% (episode['name'], episode['season'], episode['episode'])
+	seasonFormat = '%s/%s Season %s/'% (episode['name'], episode['name'], episode['season'])
+	
+	newMediaitems = newnameMediaitems(episode['media_items'])
+	for item in newMediaitems:
+		print(showDir + episode['original'] + '/' + item[0])
+		print(showDir + seasonFormat + episodeFormat + item[1] + '\n')
 	
 	if episode['subtitles']: 
-		newSubtitles = getSubtitles(episode['subtitles'])
-		print(newSubtitles)
+		newSubtitles = newnameSubtitles(episode['subtitles'])
+		for item in newSubtitles:
+			print(showDir + episode['original'] + '/' + item[0])
+			print(showDir + seasonFormat + episodeFormat + item[1] + '\n')
 
 
 
