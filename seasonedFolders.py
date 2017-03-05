a08f8f4cae346c321edd2e5a78dc6983b4ceba62
KevinMidboe <kevin.midboe@gmail.com>
2017-03-05 17:27:25 +0100

    Added a loop function to run every 10 seconds

--- seasonedFolders.py
+++ seasonedFolders.py
@@ -3,16 +3,20 @@
 # @Author: KevinMidboe
 # @Date:   2017-03-05 15:55:16
 # @Last Modified by:   KevinMidboe
-# @Last Modified time: 2017-03-05 15:56:14
+# @Last Modified time: 2017-03-05 17:25:50
+
+from time import sleep
 
 from findStray import findStray
 from tweetNewEpisodes import tweetNewEpisodes
 from folderCreator import findVerified
 
 def main():
-	findStray()
-	tweetNewEpisodes()
-	findVerified()
+	while True:
+		findStray()
+		tweetNewEpisodes()
+		findVerified()
+		sleep(10)
 
 if __name__ == '__main__':
 	main()
