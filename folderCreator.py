561e88933c719405b6ce1fbb5655553f9977883c
KevinMidboe <kevin.midboe@gmail.com>
2017-03-05 15:00:28 +0100

    Unpacks jsonobjects to lists. Added functions for iterating through subtitles and media_items, and renaming them.

--- folderCreator.py
+++ folderCreator.py
@@ -3,20 +3,77 @@
 # @Author: KevinMidboe
 # @Date:   2017-03-05 13:52:45
 # @Last Modified by:   KevinMidboe
-# @Last Modified time: 2017-03-05 13:55:09
+# @Last Modified time: 2017-03-05 14:59:26
 
-import sqlite3
+import sqlite3, json
+from re import sub
 
 dbPath = 'shows.db'
 
-def findVerified():
+def unpackEpisodes():
 	conn = sqlite3.connect(dbPath)
 	c = conn.cursor()
 
-	c.execute('SELECT * FROM stray_episodes WHERE verified = 1')
+	cursor = c.execute('SELECT * FROM stray_episodes WHERE verified = 1')
+	episodeList = []
+	for row in c.fetchall():
+		columnNames = [description[0] for description in cursor.description]
+		
+		episodeDict = dict.fromkeys(columnNames)
+		
+		for i, key in enumerate(episodeDict.keys()):
+			episodeDict[key] = row[i]
 
-	for item in c.fetchone():
-		print(item)
+		episodeList.append(episodeDict)
+	
+	conn.close()
+
+	return episodeList
+
+
+def createFolders(episode):
+	
+
+
+def getMediaitems(media_items):
+	# media_items = [['New.Girl.S06E18.720p.HDTV.x264-EZTV.srt', '-EZTV', 'nl'], ['New.Girl.S06E18.720p.HDTV.x264-FLEET.srt', '-FLEET', 'en']]
+	media_items = json.loads(media_items)
+
+	returnList = []
+	for item in media_items:
+		returnList.append(sub(item[1], '', item[0]))
+
+	return returnList
+
+def getSubtitles(subtitles):
+	subtitles = json.loads(subtitles)
+
+	returnList = []
+	for item in subtitles:
+		returnList.append(sub(item[1], '.' + item[2], item[0]))
+
+	return returnList
+
+
+def moveFiles(episode):
+	newMediaitems = getMediaitems(episode['media_items'])
+	print(newMediaitems)
+	
+	if episode['subtitles']: 
+		newSubtitles = getSubtitles(episode['subtitles'])
+		print(newSubtitles)
+
+
+
+def findVerified():
+	episodes = unpackEpisodes()
+	if episodes:
+		for episode in episodes:
+			createFolders(episode)
+			moveFiles(episode)
+	
+	# for item in c.fetchall():
+		# print(item)
 
 def main():
 	findVerified()
