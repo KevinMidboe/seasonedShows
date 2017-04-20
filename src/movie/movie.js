df90cc103795e5eb4be1226c1fa17db7a2c0f756
KevinMidboe <kevin.midboe@gmail.com>
2017-04-20 22:27:24 +0200

    Added a movie class so that different movie types (plex vs tmdb) can be converted into a universal movie class

---
+++ src/movie/movie.js
@@ -0,0 +1,12 @@
+class Movie {
+	constructor(title, year) {
+		this.id = undefined;
+		this.title = title;
+		this.year = year;
+		this.release_date = undefined;
+		this.library = undefined;
+		this.type = undefined;
+	}
+}
+
+module.exports = Movie;
