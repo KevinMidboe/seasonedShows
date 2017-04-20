b9683e7b25a3cdf32880326b9f6e90cd972a9f9a
KevinMidboe <kevin.midboe@gmail.com>
2017-04-20 22:18:38 +0200

    Added function for searching through media on plex with its api endpoint. The returned data is then passed to a convert function so a movie class only holds information we want.

--- src/plex/plexRepository.js
+++ src/plex/plexRepository.js
@@ -1,5 +1,6 @@
 const assert = require('assert');
-var request = require('request');
+const convertPlexToMovie = require('src/plex/convertPlexToMovie');
+var rp = require('request-promise');
 
 class PlexRepository {
 
@@ -11,12 +12,21 @@ class PlexRepository {
 	}
 
 	searchMedia(query) {
-		request
-			.get('10.0.0.41:32400/search?query=' + query)
-			.on('response', function(response) {
-				console.log(response.statusCode);
-				return response;
-			})
+		var options = {
+			uri: 'http://10.0.0.41:32400/search?query=' + query,
+			headers: {
+				'Accept': 'application/json'
+			},
+			json: true
+		}
+
+		return rp(options)
+		  .then((result) => {
+		  	return result.MediaContainer.Metadata.map(convertPlexToMovie);
+		  })
+		  .catch((err) => {
+		  	throw new Error(err);
+		  })
 	}
 }
 
