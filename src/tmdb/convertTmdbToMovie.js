98161f0616f36020631a7ae4baacd1922eb63b91
KevinMidboe <kevin.midboe@gmail.com>
2017-04-21 16:56:50 +0200

    Now handles movies and tv shows

--- src/tmdb/convertTmdbToMovie.js
+++ src/tmdb/convertTmdbToMovie.js
@@ -2,13 +2,25 @@ const Movie = require('src/movie/movie');
 
 function convertTmdbToMovie(tmdbMovie) {
 	const movie = new Movie();
-	movie.title = tmdbMovie.title;
-	movie.type = 'movie';
+	if (tmdbMovie.media_type === 'movie') {
+		movie.title = tmdbMovie.title;
+		movie.type = tmdbMovie.media_type;
 
-	if (tmdbMovie.release_date !== undefined) {
-		movie.release_date = new Date(tmdbMovie.release_date);
-		movie.year = movie.release_date.getFullYear();
+		if (tmdbMovie.release_date !== undefined) {
+			movie.release_date = new Date(tmdbMovie.release_date);
+			movie.year = movie.release_date.getFullYear();
+		}
+	} else if (tmdbMovie.media_type === 'tv') {
+		movie.title = tmdbMovie.name;
+		movie.type = 'show';
+		if (tmdbMovie.first_air_date !== undefined) {
+			movie.release_date = new Date(tmdbMovie.first_air_date);
+			movie.year = movie.release_date.getFullYear();
+		}
 	}
+
+	movie.poster = tmdbMovie.poster_path;
+	movie.background = tmdbMovie.backdrop_path;
 
 	return movie;
 }
