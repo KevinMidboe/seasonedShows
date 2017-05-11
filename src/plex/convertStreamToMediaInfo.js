3a83b98df0d69293069149f1df2b08e65c376546
KevinMidboe <kevin.midboe@gmail.com>
2017-05-10 19:59:25 -0600

    Function that converts json object about stream from plex format to wanted custom format.

---
+++ src/plex/convertStreamToMediaInfo.js
@@ -0,0 +1,19 @@
+const MediaInfo = require('src/media_classes/mediaInfo');
+
+function convertStreamToMediaInfo(plexStream) {
+	const mediaInfo = new MediaInfo();
+	
+	mediaInfo.duration = plexStream.duration;
+	mediaInfo.height = plexStream.height;
+	mediaInfo.width = plexStream.width;
+	mediaInfo.bitrate = plexStream.bitrate;
+	mediaInfo.resolution = plexStream.videoResolution;
+	mediaInfo.framerate = plexStream.videoFrameRate;
+	mediaInfo.protocol = plexStream.protocol;
+	mediaInfo.container = plexStream.container;
+	mediaInfo.audioCodec = plexStream.audioCodec;
+
+	return mediaInfo;
+}
+
+module.exports = convertStreamToMediaInfo;
