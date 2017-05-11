a5ecaff00e3bc26d7fb6f1a1673597bf0035a8f8
KevinMidboe <kevin.midboe@gmail.com>
2017-05-10 20:16:05 -0600

    Routes request to correct function in plexRepo and if error, returns correct error.

---
+++ src/webserver/controllers/plex/plexPlaying.js
@@ -0,0 +1,14 @@
+const PlexRepository = require('src/plex/plexRepository');
+const plexRepository = new PlexRepository();
+
+function playingController(req, res) {
+	plexRepository.nowPlaying()
+	.then((movies) => {
+		res.send(movies);
+	})
+	.catch((error) => {
+		res.status(500).send({success: false, error: error.message });
+	})
+}
+
+module.exports = playingController;
