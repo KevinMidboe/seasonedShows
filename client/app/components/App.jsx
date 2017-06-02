f548dba7e12eb2e23ab2dc4421b851d7e35dce4e
KevinMidboe <kevin.midboe@gmail.com>
2017-06-02 12:04:43 +0200

    Setup of yarn and package.json. Also added first page which queries api for info

---
+++ client/app/components/App.jsx
@@ -0,0 +1,23 @@
+/*
+	./app/components/App.jsx
+	
+	<FetchData url={"https://apollo.kevinmidboe.com/api/v1/plex/playing"} />
+*/
+import React from 'react';
+import FetchData from './FetchData.js';
+import ListStrays from './ListStrays.jsx'
+
+export default class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<div style={{textAlign: 'center'}}>
+					<h1>Welcome to Seasoned</h1>
+				</div>
+					<ListStrays />
+
+					<FetchData />
+			</div>
+		);
+	}
+}
