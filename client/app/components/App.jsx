d2c456305ab206e9328b7f098db28ebe4dfcfdba
KevinMidboe <kevin.midboe@gmail.com>
2017-08-05 17:04:44 +0200

    Added style to background and main object

--- client/app/components/App.jsx
+++ client/app/components/App.jsx
@@ -8,11 +8,23 @@ import FetchData from './FetchData.js';
 import ListStrays from './ListStrays.jsx'
 import SearchRequest from './SearchRequest.jsx';
 
+import WebFont from 'webfontloader';
+
+WebFont.load({
+  google: {
+    families: ['Titillium Web:300,400,700', 'sans-serif']
+  }
+});
+	var background = {
+      backgroundColor: '#fafafa'
+    }
+
 export default class App extends React.Component {
+
 	render() {
 		return (
-			<div>
-				<div style={{textAlign: 'center'}}>
+			<div style={background}>
+				<div>
 					<h1>Welcome to Seasoned</h1>
 				</div>
 					<ListStrays />
