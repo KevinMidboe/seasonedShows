149b05ef28edeb669f1397a8973eb48b7b7d0495
KevinMidboe <kevin.midboe@gmail.com>
2017-09-27 17:55:07 +0200

    Calling function for fetching all the requested items.

---
+++ seasoned_api/src/webserver/controllers/plex/fetchRequested.js
@@ -0,0 +1,23 @@
+const RequestRepository = require('src/plex/requestRepository.js');
+const requestRepository = new RequestRepository();
+
+/**
+ * Controller: Retrieves search history of a logged in user
+ * @param {Request} req http request variable
+ * @param {Response} res
+ * @returns {Callback}
+ */
+function historyController(req, res) {
+  const user = req.loggedInUser;
+  console.log('here')
+
+  requestRepository.fetchRequested()
+  .then((requestedItems) => {
+    res.send({ success: true, requestedItems });
+  })
+  .catch((error) => {
+    res.status(401).send({ success: false, error: error.message });
+  });
+}
+
+module.exports = historyController;
