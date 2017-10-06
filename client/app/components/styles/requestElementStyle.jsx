c954bd48748db73dc2936bed9b02f40c331796d1
KevinMidboe <kevin.midboe@gmail.com>
2017-10-06 12:08:06 +0200

    Added stylesheet for requestElement

---
+++ client/app/components/styles/requestElementStyle.jsx
@@ -0,0 +1,23 @@
+
+export default {
+	bodyDiv: {
+		display: 'flex',
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		flexFlow: 'row wrap',
+	},
+
+	wrappingDiv: {
+
+	},
+
+	requestPoster: {
+		height: '150px',
+	},
+
+	infoDiv: {
+		marginTop: 0,
+		marginLeft: '10px',
+		float: 'right',
+	},
+}
