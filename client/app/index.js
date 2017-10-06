bd74e1dee11bc63e278a799d6a2d3b65529d8967
KevinMidboe <kevin.midboe@gmail.com>
2017-10-06 12:03:16 +0200

    The first thing the index does is to load the app file in a hashrouter object and send it to the element with id root.

--- client/app/index.js
+++ client/app/index.js
@@ -2,14 +2,19 @@
 * @Author: KevinMidboe
 * @Date:   2017-06-01 21:08:55
 * @Last Modified by:   KevinMidboe
-* @Last Modified time: 2017-06-01 21:34:32
+* @Last Modified time: 2017-10-05 13:47:37
 
     ./client/index.js
     which is the webpack entry file
 */
 
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render((
+	<HashRouter>
+		<App />
+	</HashRouter>
+), document.getElementById('root'));
