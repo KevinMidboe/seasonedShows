6dd45cf89e608202192c89a52baf895edd2da05e
KevinMidboe <kevin.midboe@gmail.com>
2017-10-06 12:05:00 +0200

    Empty header file.

---
+++ client/app/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <header>
+  </header>
+)
+
+export default Header
