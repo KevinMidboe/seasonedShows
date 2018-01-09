519eba8fda175042846ad7cc216cfdd763e6474e
KevinMidboe <kevin.midboe@gmail.com>
2018-01-09 16:36:24 +0100

    Added some css for admin component that defines the styles for parent elements of imported components.

--- client/app/components/styles/adminComponent.jsx
+++ client/app/components/styles/adminComponent.jsx
@@ -1,8 +1,16 @@
 export default {
 	sidebar: {
-		float: 'left',
+      float: 'left',
+		width: '18%',
+      minWidth: '250px',
+      fontFamily: '"Open Sans", sans-serif',
+      fontSize: '14px',
+      borderRight: '2px solid #f2f2f2',
 	},
 	selectedObjectPanel: {
-		float: 'left',
+      width: '80%',
+      float: 'right',
+      fontFamily: '"Open Sans", sans-serif',
+      marginTop: '1em',
 	}
 }
