89ec9d496b65cdb91eccec5acd6c48dc543b52da
Kevin Midboe <kevin.midboe@gmail.com>
2018-02-05 13:42:35 +0100

    Changed the ip addres of plex server

--- seasoned_api/src/plex/plexRepository.js
+++ seasoned_api/src/plex/plexRepository.js
@@ -7,7 +7,7 @@ class PlexRepository {
 
 	searchMedia(query) {
 		var options = {
-			uri: 'http://10.0.0.42:32400/search?query=' + query,
+			uri: 'http://10.0.0.44:32400/search?query=' + query,
 			headers: {
 				'Accept': 'application/json'
 			},
@@ -31,7 +31,7 @@ class PlexRepository {
 
 	nowPlaying() {
 		var options = {
-			uri: 'http://10.0.0.42:32400/status/sessions',
+			uri: 'http://10.0.0.44:32400/status/sessions',
 			headers: {
 				'Accept': 'application/json'
 			},
