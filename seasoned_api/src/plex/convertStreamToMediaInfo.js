dda1db6c5f63675bee3e4c2b1779c6586540e3e0
KevinMidboe <kevin.midboe@gmail.com>
2018-02-07 13:50:05 +0100

    Linted all plex scripts.

--- seasoned_api/src/plex/convertStreamToMediaInfo.js
+++ seasoned_api/src/plex/convertStreamToMediaInfo.js
@@ -1,22 +1,22 @@
 const MediaInfo = require('src/media_classes/mediaInfo');
 
 function convertStreamToMediaInfo(plexStream) {
-	const mediaInfo = new MediaInfo();
-	
-	mediaInfo.duration = plexStream.duration;
-	mediaInfo.height = plexStream.height;
-	mediaInfo.width = plexStream.width;
+   const mediaInfo = new MediaInfo();
 
-	if (plexStream.bitrate) {
-		mediaInfo.bitrate = plexStream.bitrate;
-	}
-	mediaInfo.resolution = plexStream.videoResolution;
-	mediaInfo.framerate = plexStream.videoFrameRate;
-	mediaInfo.protocol = plexStream.protocol;
-	mediaInfo.container = plexStream.container;
-	mediaInfo.audioCodec = plexStream.audioCodec;
+   mediaInfo.duration = plexStream.duration;
+   mediaInfo.height = plexStream.height;
+   mediaInfo.width = plexStream.width;
 
-	return mediaInfo;
+   if (plexStream.bitrate) {
+      mediaInfo.bitrate = plexStream.bitrate;
+   }
+   mediaInfo.resolution = plexStream.videoResolution;
+   mediaInfo.framerate = plexStream.videoFrameRate;
+   mediaInfo.protocol = plexStream.protocol;
+   mediaInfo.container = plexStream.container;
+   mediaInfo.audioCodec = plexStream.audioCodec;
+
+   return mediaInfo;
 }
 
 module.exports = convertStreamToMediaInfo;
--- seasoned_api/src/plex/plexRepository.js
+++ seasoned_api/src/plex/plexRepository.js
@@ -1,87 +1,80 @@
-const assert = require('assert');
 const convertPlexToSeasoned = require('src/plex/convertPlexToSeasoned');
 const convertPlexToStream = require('src/plex/convertPlexToStream');
-var rp = require('request-promise');
-
-const PLEX_METHODS = ['lookup', 'playing']
+const rp = require('request-promise');
 
 class PlexRepository {
+   inPlex(tmdbResult) {
+      return Promise.resolve()
+         .then(() => this.search(tmdbResult.title))
+         .then(plexResult => this.compareTmdbToPlex(tmdbResult, plexResult));
+   }
 
-	search(query, callback) {
-		var options = {
-			uri: 'http://10.0.0.44:32400/search?query=' + query,
-			headers: {
-				'Accept': 'application/json'
-			},
-			json: true
-		}
+   search(query) {
+      const options = {
+         uri: `http://10.0.0.44:32400/search?query=${query}`,
+         headers: {
+            Accept: 'application/json',
+         },
+         json: true,
+      };
 
-		return rp(options)
-		.then((result) => this.mapResults(result))
-		.then(([mappedResults, resultCount]) => {
-			return { 'results': mappedResults, 'total_results': resultCount }
-		})
-	}
+      return rp(options)
+         .then(result => this.mapResults(result))
+         .then(([mappedResults, resultCount]) => ({ results: mappedResults, total_results: resultCount }));
+   }
 
-	compareTmdbToPlex(tmdb, plexResult) {
-		return Promise.resolve()
-		.then(() => {
-			plexResult.results.map((plexItem) => {
-				if (tmdb.title === plexItem.title && tmdb.year === plexItem.year)
-					tmdb.matchedInPlex = true;
-			})
-			return tmdb
-		})
-	}
+   static compareTmdbToPlex(tmdb, plexResult) {
+      return Promise.resolve()
+         .then(() => {
+            plexResult.results.map((plexItem) => {
+               if (tmdb.title === plexItem.title && tmdb.year === plexItem.year) { tmdb.matchedInPlex = true; }
+               return tmdb;
+            });
+            return tmdb;
+         });
+   }
 
-	inPlex(tmdbResult) {
-		return Promise.resolve()
-		.then(() => this.search(tmdbResult.title))
-		.then((plexResult) => this.compareTmdbToPlex(tmdbResult, plexResult))
-	}
+   static mapResults(response) {
+      return Promise.resolve()
+         .then(() => {
+            if (!response.MediaContainer.hasOwnProperty('Metadata')) return [[], 0];
 
-	mapResults(response) {
-		return Promise.resolve()
-		.then(() => {
-			if (! response.MediaContainer.hasOwnProperty('Metadata')) return [[], 0]
+            const mappedResults = response.MediaContainer.Metadata.filter((element) => {
+               return (element.type === 'movie' || element.type === 'show');
+            }).map((element) => convertPlexToSeasoned(element));
+            return [mappedResults, mappedResults.length];
+         })
+         .catch((error) => { throw new Error(error); });
+   }
 
-			const mappedResults = response.MediaContainer.Metadata.filter((element) => {
-				return (element.type === 'movie' || element.type === 'show')
-			}).map((element) => convertPlexToSeasoned(element))
-			return [mappedResults, mappedResults.length]
-		})
-		.catch((error) => {throw new Error(error)})
-	}
+   nowPlaying() {
+      const options = {
+         uri: 'http://10.0.0.44:32400/status/sessions',
+         headers: {
+            Accept: 'application/json',
+         },
+         json: true,
+      };
 
-	nowPlaying() {
-		var options = {
-			uri: 'http://10.0.0.44:32400/status/sessions',
-			headers: {
-				'Accept': 'application/json'
-			},
-			json: true
-		}
+      return rp(options)
+         .then((result) => {
+            if (result.MediaContainer.size > 0) {
+               const playing = result.MediaContainer.Video.map(convertPlexToStream);
+               return { size: Object.keys(playing).length, video: playing };
+            }
+            return { size: 0, video: [] };
+         })
+         .catch((err) => {
+            throw new Error(`Error handling plex playing. Error: ${err}`);
+         });
+   }
 
-		return rp(options)
-		  .then((result) => {
-		  	  if (result.MediaContainer.size > 0) {
-			  	var playing = result.MediaContainer.Video.map(convertPlexToStream);
-			  	return {'size': Object.keys(playing).length, 'video': playing };
-			  } else {
-			  	return { 'size': 0, 'video': [] };
-			  }
-		  })
-		  .catch((err) => {
-		  	throw new Error('Error handling plex playing. Error: ' + err);
-		  })
-	}
-
-	// multipleInPlex(tmdbResults) {
-	// 	const results = tmdbResults.results.map(async (tmdb) => {
-	// 		return this.inPlex(tmdb)
-	// 	})
-	// 	return Promise.all(results)
-	// }
+   // multipleInPlex(tmdbResults) {
+   //    const results = tmdbResults.results.map(async (tmdb) => {
+   //       return this.inPlex(tmdb)
+   //    })
+   //    return Promise.all(results)
+   // }
 }
 
 module.exports = PlexRepository;
