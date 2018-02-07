bd1e3c44557103864b369922664cf82b2d5c7e31
KevinMidboe <kevin.midboe@gmail.com>
2018-02-07 14:35:36 +0100

    Added test for checking popular shows list in tmdb.

---
+++ seasoned_api/test/system/asAUserIWantToGetPopularShows.js
@@ -0,0 +1,18 @@
+const assert = require('assert');
+const createCacheEntry = require('test/helpers/createCacheEntry');
+const resetDatabase = require('test/helpers/resetDatabase');
+const request = require('supertest-as-promised');
+const app = require('src/webserver/app');
+const popularShowsSuccess = require('test/fixtures/popular-show-success-response.json');
+
+describe('As a user I want to get popular shows', () => {
+  before(() => resetDatabase());
+  before(() => createCacheEntry('p:show::1', popularShowsSuccess));
+
+  it('should return 200 with the information', () =>
+    request(app)
+    .get('/api/v1/tmdb/list/popular?type=show')
+    .expect(200)
+    .then(response => assert.equal(response.body.results.length, 20))
+  );
+});
