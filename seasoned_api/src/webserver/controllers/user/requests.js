1a693ef92f4f30958af824586a6cd63a6ba85cad
KevinMidboe <kevin.midboe@gmail.com>
2018-03-06 19:21:55 +0100

    Our new controller for getting a users requested items.

---
+++ seasoned_api/src/webserver/controllers/user/requests.js
@@ -0,0 +1,25 @@
+const RequestRepository = require('src/plex/requestRepository.js');
+
+const requestRepository = new RequestRepository();
+
+/**
+ * Controller: Retrieves search history of a logged in user
+ * @param {Request} req http request variable
+ * @param {Response} res
+ * @returns {Callback}
+ */
+function requestsController(req, res) {
+   const user = req.loggedInUser;
+   console.log('user: ', user)
+
+   requestRepository.userRequests(user)
+      .then((requests) => {
+         res.send({ success: true, results: requests, total_results: requests.length });
+      })
+      .catch((error) => {
+         console.log(error)
+         res.status(500).send({ success: false, error: error });
+      });
+}
+
+module.exports = requestsController;
