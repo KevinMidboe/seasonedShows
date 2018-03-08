127db8b2fe6a846d6d5b781247cf9e7e895dae0d
KevinMidboe <kevin.midboe@gmail.com>
2018-03-08 10:32:19 +0100

    Errors when we have a user that was not signed in. Fixed to unpack the username value first, then pass it to the function.

--- seasoned_api/src/webserver/controllers/plex/searchRequest.js
+++ seasoned_api/src/webserver/controllers/plex/searchRequest.js
@@ -10,15 +10,16 @@ const searchHistory = new SearchHistory();
 function searchRequestController(req, res) {
    const user = req.loggedInUser;
    const { query, page, type } = req.query;
+   const username = user == undefined ? undefined : user.username;
 
    Promise.resolve()
-      .then(() => searchHistory.create(user, query))
+      .then(() => searchHistory.create(username, query))
       .then(() => requestRepository.search(query, page, type))
       .then((searchResult) => {
          res.send(searchResult);
       })
       .catch((error) => {
-         res.status(500).send({ success: false, error: error.message });
+         res.status(500).send({ success: false, error: error });
       });
 }
 
