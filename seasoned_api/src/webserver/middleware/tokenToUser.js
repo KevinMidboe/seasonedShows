e19cfb5870c31c83766166631d3885ec8a148828
KevinMidboe <kevin.midboe@gmail.com>
2019-07-25 00:24:04 +0200

    Updated formatting

--- seasoned_api/src/webserver/middleware/tokenToUser.js
+++ seasoned_api/src/webserver/middleware/tokenToUser.js
@@ -8,16 +8,16 @@ const Token = require('src/user/token');
 // curl -i -H "Authorization:[token]" localhost:31459/api/v1/user/history
 
 const tokenToUser = (req, res, next) => {
-   const rawToken = req.headers.authorization;
-   if (rawToken) {
-      try {
-         const token = Token.fromString(rawToken, secret);
-         req.loggedInUser = token.user;
-      } catch (error) {
-         req.loggedInUser = undefined;
-      }
-   }
-   next();
+  const rawToken = req.headers.authorization;
+  if (rawToken) {
+    try {
+      const token = Token.fromString(rawToken, secret);
+      req.loggedInUser = token.user;
+    } catch (error) {
+      req.loggedInUser = undefined;
+    }
+  }
+  next();
 };
 
 module.exports = tokenToUser;
