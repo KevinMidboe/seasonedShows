e5d5bdefd62b3f601317005e3d3f84ea996122ee
KevinMidboe <kevin.midboe@gmail.com>
2019-07-26 21:05:45 +0200

    Updated cache key and cleaned up formatting

--- seasoned_api/test/system/asAUserIWantToRequestAMovie.js
+++ seasoned_api/test/system/asAUserIWantToRequestAMovie.js
@@ -7,17 +7,17 @@ const createToken = require('test/helpers/createToken');
 const infoMovieSuccess = require('test/fixtures/blade_runner_2049-info-success-response.json');
 
 describe('As a user I want to request a movie', () => {
-  before(() => {
-   return resetDatabase()
-   .then(() => createUser('test_user', 'test@gmail.com', 'password'));
+  before(async () => {
+    await resetDatabase()
+    await createUser('test_user', 'test@gmail.com', 'password')
    })
-  before(() => createCacheEntry('mi:335984', infoMovieSuccess));
+  before(() => createCacheEntry('mi:335984:false', infoMovieSuccess));
 
   it('should return 200 when item is requested', () =>
     request(app)
     .post('/api/v2/request')
+    .set('authorization', createToken('test_user', 'secret'))
     .send({ id: 335984, type: 'movie' })
-    .set('Authorization', createToken('test_user', 'secret'))
     .expect(200)
   );
 });
