720fb69648a9db22a3c7214237a04243ea25ba98
KevinMidboe <kevin.midboe@gmail.com>
2019-12-22 12:45:01 +0100

    Indentation

--- seasoned_api/src/request/request.js
+++ seasoned_api/src/request/request.js
@@ -88,16 +88,16 @@ class RequestRepository {
    */
   requestFromTmdb(tmdb, ip, user_agent, username) {
     return Promise.resolve()
-    .then(() => this.database.get(this.queries.read, [tmdb.id, tmdb.type]))
-    .then(row => assert.equal(row, undefined, 'Id has already been requested'))
-    .then(() => this.database.run(this.queries.add, [tmdb.id, tmdb.title, tmdb.year, tmdb.poster, tmdb.backdrop, username, ip, user_agent, tmdb.type]))
-    .catch((error) => {
-      if (error.name === 'AssertionError' || error.message.endsWith('been requested')) {
-        throw new Error('This id is already requested', error.message);
-      }
-      console.log('Error @ request.addTmdb:', error);
-      throw new Error('Could not add request');
-    });
+      .then(() => this.database.get(this.queries.read, [tmdb.id, tmdb.type]))
+      .then(row => assert.equal(row, undefined, 'Id has already been requested'))
+      .then(() => this.database.run(this.queries.add, [tmdb.id, tmdb.title, tmdb.year, tmdb.poster, tmdb.backdrop, username, ip, user_agent, tmdb.type]))
+      .catch((error) => {
+        if (error.name === 'AssertionError' || error.message.endsWith('been requested')) {
+          throw new Error('This id is already requested', error.message);
+        }
+        console.log('Error @ request.addTmdb:', error);
+        throw new Error('Could not add request');
+      });
   }
 
   /**
