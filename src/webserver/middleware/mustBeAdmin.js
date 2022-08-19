a98797088a05d66bcc5cf68cdae255da68c70549
Kevin Midboe <kevin.midboe@gmail.com>
2022-08-19 11:22:29 +0200

    Linted and some consistency refactor on middleware

--- src/webserver/middleware/mustBeAdmin.js
+++ src/webserver/middleware/mustBeAdmin.js
@@ -1,30 +1,31 @@
 const establishedDatabase = require("../../database/database");
 
+// eslint-disable-next-line consistent-return
 const mustBeAdmin = (req, res, next) => {
   const database = establishedDatabase;
 
   if (req.loggedInUser === undefined) {
-    return res.status(401).send({
+    res.status(401).send({
       success: false,
       message: "You must be logged in."
     });
   }
+
   database
     .get(
       `SELECT admin FROM user WHERE user_name IS ?`,
       req.loggedInUser.username
     )
     .then(isAdmin => {
-      console.log(isAdmin, req.loggedInUser);
-      if (isAdmin.admin == 0) {
+      if (isAdmin.admin === 0) {
         return res.status(401).send({
           success: false,
           message: "You must be logged in as a admin."
         });
       }
-    });
 
-  return next();
+      return next();
+    });
 };
 
 module.exports = mustBeAdmin;
--- src/webserver/middleware/mustHaveAccountLinkedToPlex.js
+++ src/webserver/middleware/mustHaveAccountLinkedToPlex.js
@@ -1,33 +1,36 @@
 const establishedDatabase = require("../../database/database");
 
+/* eslint-disable consistent-return */
 const mustHaveAccountLinkedToPlex = (req, res, next) => {
   const database = establishedDatabase;
   const { loggedInUser } = req;
 
-  if (loggedInUser === undefined) {
+  if (loggedInUser === null) {
     return res.status(401).send({
       success: false,
       message: "You must have your account linked to a plex account."
     });
   }
+
   database
     .get(
       `SELECT plex_userid FROM settings WHERE user_name IS ?`,
       loggedInUser.username
     )
     .then(row => {
-      const plexUserId = row?.plex_userid;
-
-      if (plexUserId === null || plexUserId === undefined) {
+      const plexUserId = row.plex_userid;
+      if (plexUserId === null) {
         return res.status(403).send({
           success: false,
           message:
             "No plex account user id found for your user. Please authenticate your plex account at /user/authenticate."
         });
       }
+
       req.loggedInUser.plexUserId = plexUserId;
-      return next();
+      next();
     });
 };
+/* eslint-enable consistent-return */
 
 module.exports = mustHaveAccountLinkedToPlex;
