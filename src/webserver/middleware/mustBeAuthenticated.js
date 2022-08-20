ff410194edb8aaf592606c4b7dbf1ae566e7e175
Kevin Midboe <kevin.midboe@gmail.com>
2022-08-20 17:17:31 +0200

    Updated authentication middleware to handle checks consitenctly

--- src/webserver/middleware/mustBeAuthenticated.js
+++ src/webserver/middleware/mustBeAuthenticated.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line consistent-return
 const mustBeAuthenticated = (req, res, next) => {
-  if (req.loggedInUser === undefined) {
+  if (!req.loggedInUser) {
     return res.status(401).send({
       success: false,
       message: "You must be logged in."
